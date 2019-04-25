7d66f4e6dd7870ba22c48e7734925e5f88b1eec9
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-04-25 23:40:27 +0430

    add another code to python triangle

--- python triangle.py
+++ python triangle.py
@@ -4,3 +4,10 @@ i = 1
 while i <= n:
     print("*"*i)
     i += 1
+
+
+"""n = int(input("Please Enter A Number:"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end ="")
+    print()"""
