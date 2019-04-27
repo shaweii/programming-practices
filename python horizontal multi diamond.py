486425e113348cdb80aed4a7719a3d101a0fc1a1
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-04-28 01:47:29 +0430

    python vertical multi diamond

--- python horizontal multi diamond.py
+++ python horizontal multi diamond.py
@@ -1,8 +1,8 @@
-print("python vertical multi diamond.")
+print("python horizontal multi diamond.")
 n = int(input("Please Enter A Number:"))
-m = int(input("Please Enter A Number for To print the number of Vertical rhombuses:"))
+m = int(input("Please Enter A Number for To print the number of horizontal rhombuses:"))
 k = 0
-for s in range(1,m+1):
+for s in range(1,(m-i)+1):
   for i in range(1, n+1):
     for j in range(1, (n-i)+1):
         print(end="  ")
