9fa02db939f6582ef0fe74939fdc6bfa2ca19d8d
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-04-28 01:19:58 +0430

    python equilateral triangle

--- python reverse equilateral triangle.py
+++ python reverse equilateral triangle.py
@@ -1,8 +1,8 @@
 print("python equilateral triangle.")
 n = int(input("Please Enter A Number:"))
 k = 0
-for i in range(1, n+1):
-    for j in range(1, (n-i)+1):
+for i in range(n,0,-1):
+    for j in range(1,(n-i)+1):
         print(end="  ")
     while k != (2*i-1):
         print("* ", end="")
