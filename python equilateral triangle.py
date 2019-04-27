f4fb5b70d8c931cae2f0c2632ad1919d4f9ac389
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-04-27 23:08:56 +0430

    python equilateral triangle

--- python equilateral triangle.py
+++ python equilateral triangle.py
@@ -1,9 +1,12 @@
 print("python equilateral triangle.")
 n = int(input("Please Enter A Number:"))
-for i in range(1,n+1):
-    for j in range(n,0,-1):
-        if(j < i):
-            print(' ', end=' ')
-        else:
-            print('*', end=' ')
+k = 0
+
+for i in range(1, n+1):
+    for space in range(1, (n-i)+1):
+        print(end="  ")
+    while k != (2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
     print()
