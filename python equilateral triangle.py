861effce6dddcd29525d56400ee54c73f6a33e3d
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 17:50:09 +0430

    Python equilateral triangle

--- python equilateral triangle.py
+++ python equilateral triangle.py
@@ -4,7 +4,7 @@ for i in range(1, n+1):
     for j in range(1, (n-i)+1):
         print(end="  ")
     for k in range(0,2*i-1):
-        print("*", end="")
+        print("* ", end="")
     for j in range(1, (n-i)+1):
         print(end="  ")
     print()
