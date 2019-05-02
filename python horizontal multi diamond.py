778ec32ab7159b8230df970d6b592b9198108e69
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 16:07:37 +0430

    Python horizontal multi diamond

--- python horizontal multi diamond.py
+++ python horizontal multi diamond.py
@@ -1,22 +1,24 @@
-print("python horizontal multi diamond.")
-n = int(input("Please Enter A Number:"))
-m = int(input("Please Enter A Number for To print the number of horizontal rhombuses:"))
-k = 0
-for s in range(1,(m-i)+1):
-  for i in range(1, n+1):
-    for j in range(1, (n-i)+1):
-        print(end="  ")
-    while k != (2*i-1):
-        print("* ", end="")
-        k = k + 1
-    k = 0
+print("Python horizontal multi diamond.")
+n = int(input("Please enter a number:"))
+y = int(input("Please enter a number for print the number of horizontalrhombuses:"))
+for i in range(1, n+1):
+    for x in range(1,y+1):
+        for j in range(1, (n-i)+1):
+            print(end="  ")
+        for k in range(0,2*i-1):
+            print("* ", end="")
+        for j in range(1, (n-i)+1):
+            print(end="  ")
     print()
-  for i in range(n-1,0,-1):
-    for j in range(1,(n-i)+1):
-        print(end="  ")
-    while k != (2*i-1):
-        print("* ", end="")
-        k = k + 1
-    k = 0
-   
+
+for i in range(n-1,0,-1):
+    for x in range(1,y+1):
+        for j in range(1,(n-i)+1):
+            print(end="  ")
+        for k in range(0,2*i-1):
+            print("* ", end="")
+        for j in range(1,(n-i)+1):
+            print(end="  ")
     print()
+
+
