c371387cfed3e510ee569aab2b143a2ce491273d
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 16:19:46 +0430

    Python flipped reverse triangle

--- python filpped reverse triangle.py
+++ python filpped reverse triangle.py
@@ -1,5 +1,5 @@
-print("python flipped triangle.")
-n = int(input("Please Enter A Number:"))
+print("Python flipped reverse triangle.")
+n = int(input("Please enter a number: "))
 for i in range(1, n+1):
     for j in range(1, n+1):
         if(j < i):
