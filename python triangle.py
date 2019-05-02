62b2628cfdee6a054c59f1bf519086abb252ff02
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 19:30:10 +0430

    triangle

--- python triangle.py
+++ python triangle.py
@@ -1,15 +1,23 @@
 print("Python triangle.")
 n = int(input("Please enter a number: "))
-i = 1
+f = open("triangle" ,"w")
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        f.write("*")
+    f.write("\n")
+print("Done")  
+
+     
+  
+
+
+
+
+"""i = 1
 while i <= n:
     print("*"*i)
     i += 1
+f = open("demofile.txt", "a")
+"""
 
 
-"""
-n = int(input("Please Enter A Number:"))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print("*",end ="")
-    print()
-"""
