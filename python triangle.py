a459bd495dfcde5c8c099c887995967f63c43101
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 16:40:21 +0430

    python triangle

--- python triangle.py
+++ python triangle.py
@@ -1,13 +1,15 @@
-print("python triangle.")
-n = int(input("Please Enter A Number:"))
+print("Python triangle.")
+n = int(input("Please enter a number:"))
 i = 1
 while i <= n:
     print("*"*i)
     i += 1
 
 
-"""n = int(input("Please Enter A Number:"))
+"""
+n = int(input("Please Enter A Number:"))
 for i in range(1,n+1):
     for j in range(1,i+1):
         print("*",end ="")
-    print()"""
+    print()
+"""
