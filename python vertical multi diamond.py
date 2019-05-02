44782daea8429e12d91aff93a354ad44bd7eb761
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 16:15:28 +0430

    Python vertical multi diamond

--- python vertical multi diamond.py
+++ python vertical multi diamond.py
@@ -1,7 +1,24 @@
-print("python vertical multi diamond.")
-n = int(input("Please Enter A Number:"))
-m = int(input("Please Enter A Number for To print the number of Vertical rhombuses:"))
-k = 0
+print("Python vertical multi diamond.")
+n = int(input("Please enter a number: "))
+m = int(input("Please enter a number for print the number of Vertical rhombuses: "))
+for s in range(1,m+1):
+  for i in range(1, n+1):
+    for j in range(1, (n-i)+1):
+        print(end="  ")
+    for k in range(0,2*i-1):
+        print("* ", end="")
+    print()
+  for i in range(n-1,0,-1):
+    for j in range(1,(n-i)+1):
+        print(end="  ")
+    for k in range(0,2*i-1):
+        print("* ", end="")
+    print()
+"""
+another code for this style.
+print("Python vertical multi diamond.")
+n = int(input("Please enter a number: "))
+m = int(input("Please enter a number for print the number of Vertical rhombuses: "))
 for s in range(1,m+1):
   for i in range(1, n+1):
     for j in range(1, (n-i)+1):
@@ -18,5 +35,5 @@ for s in range(1,m+1):
         print("* ", end="")
         k = k + 1
     k = 0
-   
     print()
+    """
