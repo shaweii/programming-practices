d35b2eb8a6d3786b16d47077d93b568a8478c446
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 17:52:06 +0430

    delete something

--- test.py
+++
@@ -1,5 +0,0 @@
-"""print("using start, stop, and step arguments in Python range() function")
-print("Printing All odd numbers between 1 and 10 using range()")
-for i in range(1, 10, 2):
-    print(i, end=', ')"""
- 
