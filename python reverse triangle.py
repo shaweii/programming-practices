a6ed948a6269da0ce49709f8729bcfbdbd40a39b
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-02 19:48:09 +0430

    Python reverse triangle

--- python reverse triangle.py
+++ python reverse triangle.py
@@ -1,6 +1,8 @@
 print("Python reverse triangle.")
 n = int(input("Please enter a number: "))
+f = open("reverse triangle.txt" ,"w")
 for i in range(n,0,-1):
     for j in range(0,i):
-        f.w("*",end ="")
-    print()
+        f.write("*")
+    f.write("\n")
+print("Done")
