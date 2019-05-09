5bc3ec4fff0f602ba819cd74418821b0ddd7a975
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-09 18:23:44 +0430

    add item to dimond bg

--- background/Python diamond.bg.charecter.py
+++ background/Python diamond.bg.charecter.py
@@ -1,13 +1,13 @@
 print("Python diamond triangle.")
 ch = input("Please enter a charecter for background: ")
+char = input("Please enter a charecter for diamond: ")
 def diamond(n):
     x = ""
     for i in range(1, n+1):
         for j in range(1, (n-i)+1):
-            #ch = chr(num)
             x = x + ch + " "
         for k in range(0,2*i-1):
-            x = x + "- " 
+            x = x + char + " " 
         for j in range(1, (n-i)+1):
             x = x + ch + " "
         x = x + "\n"
@@ -16,10 +16,9 @@ def diamond_reverse(n):
     y = ""
     for i in range(n-1,0,-1):
         for j in range(1,(n-i)+1):
-            #ch = chr(num)
             y = y + ch + " "
         for k in range(0,2*i-1):
-            y = y + "- " 
+            y = y + char + " " 
         for j in range(1,(n-i)+1):
             y = y + ch + " "
         y = y + "\n"
