eac9caa2243ee426579febc985d57999fb6f03f0
Sheyda Allahweisi <sheydaallahweisi@Sheydas-MacBook-Air.local>
2019-05-09 19:38:20 +0430

    nothing

--- background/Python diamond.bg.charecter.py
+++ background/Python diamond.bg.charecter.py
@@ -1,28 +1,28 @@
 print("Python diamond triangle.")
-ch = input("Please enter a charecter for background: ")
-char = input("Please enter a charecter for diamond: ")
-def diamond(n):
+def diamond(n,bc,fc):
     x = ""
     for i in range(1, n+1):
         for j in range(1, (n-i)+1):
-            x = x + ch + " "
+            x = x + bc + " "
         for k in range(0,2*i-1):
-            x = x + char + " " 
+            x = x + fc + " " 
         for j in range(1, (n-i)+1):
-            x = x + ch + " "
+            x = x + bc + " "
         x = x + "\n"
     return x       
-def diamond_reverse(n):
+def diamond_reverse(n,bc,fc):
     y = ""
     for i in range(n-1,0,-1):
         for j in range(1,(n-i)+1):
-            y = y + ch + " "
+            y = y + bc + " "
         for k in range(0,2*i-1):
-            y = y + char + " " 
+            y = y + fc + " " 
         for j in range(1,(n-i)+1):
-            y = y + ch + " "
+            y = y + bc + " "
         y = y + "\n"
-    return diamond(a) + y
+    return diamond(a,bc,fc) + y
+bc = input("Please enter a charecter for background: ")
+fc = input("Please enter a charecter for diamond style: ")
 a = int(input("Enter a number: "))
-print(diamond_reverse(a))
+print(diamond_reverse(a,bc,fc))
 
---
+++ function/Python horizontal multi diamond.func.py
@@ -0,0 +1,32 @@
+print("Python vertical multi diamond.")
+def diamond(n):
+    x = ""
+    for i in range(1, n+1):
+        for j in range(1, (n-i)+1):
+            x = x + "  "
+        for k in range(0,2*i-1):
+            x = x + "* "
+        for j in range(1, (n-i)+1):
+            x = x + " "
+        x = x + "\n"
+    return x       
+def diamond_reverse(n):
+    y = ""
+    for i in range(n-1,0,-1):
+        for j in range(1,(n-i)+1):
+            y = y + "  "
+        for k in range(0,2*i-1):
+            y = y + "* "
+        for j in range(1,(n-i)+1):
+            y = y + " "
+        y = y + "\n"
+    return diamond(a) + y
+def mullti_dimond_vertical(m):
+    z =""
+    for s in range(1,m+1):
+        z = diamond_reverse(a) + z
+    return z 
+
+a = int(input("Enter a number: "))
+b = int(input("Please enter a number for print the number of Vertical rhombuses: "))
+print(mullti_dimond_vertical(b))
